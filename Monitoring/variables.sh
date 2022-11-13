ecd2f72c19997e4f3ccb12bb943fd14d6377da73
NktSH <49846495+NktSH@users.noreply.github.com>
2022-11-14 00:34:35 +0300

    Create variables.sh

---
+++ Monitoring/variables.sh
@@ -0,0 +1,5 @@
+#Ollo monitoring variables template 
+COS_BIN_NAME=insert_path_to_nibid_binary # example: /root/go/bin/ollod or /home/user/go/bin/ollod
+COS_PORT_RPC=insert_port_here              # default: 26657
+COS_VALOPER=insert_valoper_address         # example: ollovaloper1234545636767376535673
+LOGENTRY=ollo
